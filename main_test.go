ccf88163fe83d9cbcc0682a0119d3ce7b0e12fe3
Patrick Black <info@patrickblackjr.com>
2025-03-28 20:29:29 -0500

    ?

--- main_test.go
+++ main_test.go
@@ -1,50 +1,36 @@
 package main
 
-import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+// func TestFailure(t *testing.T) {
+// 	gin.SetMode(gin.TestMode)
+// 	mockedHttpClient := mock.NewMockedHTTPClient(mock.WithRequestMatchHandler(
+// 		mock.GetOctocat,
+// 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 			http.Error(w, "not found", http.StatusNotFound)
+// 		}),
+// 	))
+// 	client := github.NewClient(mockedHttpClient)
+// 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+// 	r := setupRouter(client, logger)
 
-	"log/slog"
-	"os"
+// 	req, _ := http.NewRequest("GET", "/", nil)
+// 	w := httptest.NewRecorder()
+// 	r.ServeHTTP(w, req)
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/go-github/v69/github"
-	"github.com/migueleliasweb/go-github-mock/src/mock"
-	"github.com/stretchr/testify/assert"
-)
+// 	assert.Equal(t, http.StatusNotFound, w.Code)
+// 	assert.Contains(t, w.Body.String(), "not found")
+// }
 
-func TestFailure(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	mockedHttpClient := mock.NewMockedHTTPClient(mock.WithRequestMatchHandler(
-		mock.GetOctocat,
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.Error(w, "not found", http.StatusNotFound)
-		}),
-	))
-	client := github.NewClient(mockedHttpClient)
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	r := setupRouter(client, logger)
+// func TestMainRoute(t *testing.T) {
+// 	gin.SetMode(gin.TestMode)
+// 	mockedHttpClient := mock.NewMockedHTTPClient(mock.WithRequestMatch(mock.GetOctocat))
+// 	client := github.NewClient(mockedHttpClient)
+// 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+// 	r := setupRouter(client, logger)
 
-	req, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+// 	req, _ := http.NewRequest("GET", "/", nil)
+// 	w := httptest.NewRecorder()
+// 	r.ServeHTTP(w, req)
 
-	assert.Equal(t, http.StatusNotFound, w.Code)
-	assert.Contains(t, w.Body.String(), "not found")
-}
-
-func TestMainRoute(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	mockedHttpClient := mock.NewMockedHTTPClient(mock.WithRequestMatch(mock.GetOctocat))
-	client := github.NewClient(mockedHttpClient)
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	r := setupRouter(client, logger)
-
-	req, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Contains(t, w.Body.String(), "ok")
-}
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Contains(t, w.Body.String(), "ok")
+// }
