3ae89447dcf29a52319075f216e56c870948eb5b
Patrick Black <info@patrickblackjr.com>
2025-04-07 19:39:44 -0500

    init unit tests

---
+++ internal/githubapi/comments_test.go
@@ -0,0 +1,105 @@
+package githubapi
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/google/go-github/v70/github"
+)
+
+func TestProcessComment(t *testing.T) {
+	type args struct {
+		event  *github.IssueCommentEvent
+		client *github.Client
+		logger *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ProcessComment(tt.args.event, tt.args.client, tt.args.logger)
+		})
+	}
+}
+
+func Test_assignUsers(t *testing.T) {
+	type args struct {
+		command string
+		event   *github.IssueCommentEvent
+		client  *github.Client
+		logger  *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assignUsers(tt.args.command, tt.args.event, tt.args.client, tt.args.logger)
+		})
+	}
+}
+
+func Test_unassignUsers(t *testing.T) {
+	type args struct {
+		command string
+		event   *github.IssueCommentEvent
+		client  *github.Client
+		logger  *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			unassignUsers(tt.args.command, tt.args.event, tt.args.client, tt.args.logger)
+		})
+	}
+}
+
+func Test_lgtm(t *testing.T) {
+	type args struct {
+		event  *github.IssueCommentEvent
+		client *github.Client
+		logger *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lgtm(tt.args.event, tt.args.client, tt.args.logger)
+		})
+	}
+}
+
+func Test_unlgtm(t *testing.T) {
+	type args struct {
+		event  *github.IssueCommentEvent
+		client *github.Client
+		logger *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			unlgtm(tt.args.event, tt.args.client, tt.args.logger)
+		})
+	}
+}
--- internal/githubapi/handler_test.go
+++ internal/githubapi/handler_test.go
@@ -2,12 +2,11 @@ package githubapi
 
 import (
 	"bytes"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
-
-	"log/slog"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v70/github"
@@ -34,3 +33,62 @@ func TestWebhookHandler(t *testing.T) {
 }
 
 // Add more tests for other event handlers
+
+func TestRegisterEventHandlers(t *testing.T) {
+	type args struct {
+		r              *gin.Engine
+		client         *github.Client
+		logger         *slog.Logger
+		processComment func(*github.IssueCommentEvent, *github.Client, *slog.Logger)
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			RegisterEventHandlers(tt.args.r, tt.args.client, tt.args.logger, tt.args.processComment)
+		})
+	}
+}
+
+func Test_handleIssueCommentEvent(t *testing.T) {
+	type args struct {
+		request        []byte
+		client         *github.Client
+		logger         *slog.Logger
+		processComment func(*github.IssueCommentEvent, *github.Client, *slog.Logger)
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handleIssueCommentEvent(tt.args.request, tt.args.client, tt.args.logger, tt.args.processComment)
+		})
+	}
+}
+
+func Test_handlePullRequestEvent(t *testing.T) {
+	type args struct {
+		request []byte
+		client  *github.Client
+		logger  *slog.Logger
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handlePullRequestEvent(tt.args.request, tt.args.client, tt.args.logger)
+		})
+	}
+}
